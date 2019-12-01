cd611043cfecff11f7ac53aa52895a2fb295373a
bashton27 <benfordashton@gmail.com>
2019-12-01 22:17:26 +0000

    Tidied up socket handler & re-implemented socket authentication

--- src/Sockets/WhoIsOnline.js
+++ src/Sockets/WhoIsOnline.js
@@ -7,35 +7,50 @@ function WhoIsOnline() {
 	const [connectedUsers, setConnectedUsers] = useState([]);
 	const [myStatusClass, setStatusClass] = useState("fa fa-user");
 	const connectedUsersList = connectedUsers.map((user) => <DropdownItem key={user.SocketID}><i className="nav-icon fa fa-circle text-success" style={{marginRight: "20px"}}></i> {user.displayName}</DropdownItem>);
-	socket.on("device connected", (docs) => {
-		console.log('getAllConnectedSockets');
-		setStatusClass("fa fa-user text-success");
-		setConnectedUsers(docs);
-		//this.setState({connectedUsers: docs});
-		//document.body.style.backgroundColor = "red"
-	});
-	socket.on("device disconnected", (docs) => {
-		console.log('USERDISCONNECTED');
-		setStatusClass("fa fa-user");
-		setConnectedUsers(docs);
-	});
 	
-	//useEffect(() => {});
+	if(localStorage.getItem("token") !== null && localStorage.getItem("roles") !== null) {
+		var token = localStorage.getItem("token");
+			console.log("Authenticating socket...");
+			var socketEventsAttached = false;
+			if(!socketEventsAttached) {
+				socket.on('connect', (error, result) => {
+					socket.emit('authentication', { token: token });
+					socket.on('authenticated', () => {
+						// Socket is authenticated
+						console.log('Socket Authenticated.');
+						socketEventsAttached = true;
+						setStatusClass("fa fa-user text-success");
+						socket.on("device connected", (docs) => {
+							console.log('device connected');
+							setConnectedUsers(docs);
+						});
+						socket.on("device disconnected", (docs) => {
+							console.log('device disconnected');
+							setStatusClass("fa fa-user");
+							setConnectedUsers(docs);
+						});
+					});
+				});
+			}
+			
+			
+		//useEffect(() => {});
 
-	return (
-		<Nav className="ml-auto" navbar>
-			<UncontrolledDropdown nav direction="down">
-				<DropdownToggle nav style={{marginRight: "20px"}}> 
-					<i className={myStatusClass}></i> Users
-				</DropdownToggle>
-				<DropdownMenu right>
-					<DropdownItem header tag="div" className="text-center"><strong>Available</strong></DropdownItem>
-					{connectedUsersList}
-					<DropdownItem divider />
-					<DropdownItem><i className="fa fa-tasks"></i> Tickets<Badge color="danger">2</Badge></DropdownItem>
-					<DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
-				</DropdownMenu>
-			</UncontrolledDropdown>
-		</Nav>
-	);
+		return (
+			<Nav className="ml-auto" navbar>
+				<UncontrolledDropdown nav direction="down">
+					<DropdownToggle nav style={{marginRight: "20px"}}> 
+						<i className={myStatusClass}></i> Users
+					</DropdownToggle>
+					<DropdownMenu right>
+						<DropdownItem header tag="div" className="text-center"><strong>Available</strong></DropdownItem>
+						{connectedUsersList}
+						<DropdownItem divider />
+						<DropdownItem><i className="fa fa-tasks"></i> Tickets<Badge color="danger">2</Badge></DropdownItem>
+						<DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
+					</DropdownMenu>
+				</UncontrolledDropdown>
+			</Nav>
+		);
+	}
 } export default WhoIsOnline;
