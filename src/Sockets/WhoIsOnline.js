2785cf945de0547509bbaa8368428fbf45a246f3
bashton27 <benfordashton@gmail.com>
2019-12-03 10:52:04 +0000

    added user status framework

--- src/Sockets/WhoIsOnline.js
+++ src/Sockets/WhoIsOnline.js
@@ -8,15 +8,13 @@
 
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Badge, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
+import { ButtonGroup, Button, Badge, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
 import io from 'socket.io-client';
 const socket = io(process.env.REACT_APP_SOCKET_URL);
 
 function WhoIsOnline() {
 	const [ connectedUsers, setConnectedUsers ] = useState([]);
 	const [ myStatusClass, setStatusClass ] = useState("fa fa-user");
-	//const connectedUsersList = Object.keys(connectedUsers).map((key, userObj) => <DropdownItem key={key}><i className="nav-icon fa fa-circle text-success" style={{marginRight: "20px"}}></i> {key.displayName}</DropdownItem>);
-	//const connectedUsersList = JSON.parse()
 	const connectedUsersList = Object.keys(connectedUsers).map((user_id) => <DropdownItem key={user_id}><i className="nav-icon fa fa-circle text-success" style={{marginRight: "20px"}}></i> {connectedUsers[user_id].displayName}</DropdownItem>);
 	const [ isLogout, setIsLogout ] = useState(false);
 
@@ -34,13 +32,11 @@ function WhoIsOnline() {
 						setStatusClass("fa fa-user text-success");
 						socket.on("device connected", (docs) => {
 							console.log('device connected');
-							console.log(docs);
 							setConnectedUsers(docs);
 						});
 						socket.on("device disconnected", (docs) => {
 							console.log('device disconnected');
-							//setStatusClass("fa fa-user");
-							//setConnectedUsers(JSON.parse(docs));
+							setConnectedUsers(docs);
 						});
 					});
 				});
@@ -48,8 +44,12 @@ function WhoIsOnline() {
 			
 			
 		//useEffect(() => {});
+
+		if(isLogout) {
+			return <Redirect push to="/login" />
+		}
 		
-		return (
+		if (!isLogout) return (
 			<Nav className="ml-auto" navbar>
 				<UncontrolledDropdown nav direction="down">
 					<DropdownToggle nav style={{marginRight: "20px"}}> 
@@ -59,7 +59,7 @@ function WhoIsOnline() {
 						<DropdownItem header tag="div" className="text-center"><strong>Notifications</strong></DropdownItem>
 						<DropdownItem divider />
 						<DropdownItem><i className="fa fa-envelope"></i> Messages<Badge color="danger">2</Badge></DropdownItem>
-						
+						<DropdownItem><i className="fa fa-tasks"></i> Tickets<Badge color="danger">2</Badge></DropdownItem>
 					</DropdownMenu>
 				</UncontrolledDropdown>
 				<UncontrolledDropdown nav direction="down">
@@ -67,10 +67,41 @@ function WhoIsOnline() {
 						<i className={myStatusClass}></i> People
 					</DropdownToggle>
 					<DropdownMenu right>
-						<DropdownItem header tag="div" className="text-center"><strong>Available</strong></DropdownItem>
-						{connectedUsersList}
+						
+						<DropdownItem header tag="div" className="text-center">
+							<strong>My Status</strong>
+						</DropdownItem>
+
+						<div className="text-center" style={{marginTop: '6px'}}>
+							<ButtonGroup size="sm">
+								<Button outline color="light" onClick={
+									()=>{
+										setStatusClass("fa fa-user text-success");
+										socket.emit("update status", { status: "Online" });
+									}
+								}><i className="nav-icon fa fa-circle text-success" style={{paddingLeft: '6px',paddingRight: '6px'}}></i></Button>
+								<Button outline color="light" onClick={
+									()=>{
+										setStatusClass("fa fa-user text-warning");
+										socket.emit("update status", { status: "Away" });
+									}
+								}><i className="nav-icon fa fa-circle text-warning" style={{paddingLeft: '6px',paddingRight: '6px'}}></i></Button>
+								<Button outline color="light" onClick={
+									()=>{
+										setStatusClass("fa fa-user text-danger");
+										socket.emit("update status", { status: "Busy" });
+									}
+								}><i className="nav-icon fa fa-circle text-danger" style={{paddingLeft: '6px',paddingRight: '6px'}}></i></Button>
+							</ButtonGroup>
+						</div>
+
 						<DropdownItem divider />
-						<DropdownItem><i className="fa fa-tasks"></i> Tickets<Badge color="danger">2</Badge></DropdownItem>
+						<DropdownItem header tag="div" className="text-center">
+							<strong>Currently Online</strong>
+						</DropdownItem>
+						
+						{connectedUsersList}
+						
 						<DropdownItem onClick={(event)=>{
 							localStorage.removeItem("token");
 							socket.close();
